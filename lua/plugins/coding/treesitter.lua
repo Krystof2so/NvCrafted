7216c22eca67457485049e8955ec029dac4f1216
Krystof2so <christo.2so@gmail.com>
2025-12-17 22:43:39 +0100

    feat(plugin): Rewriting treesitter.lua

--- lua/plugins/coding/treesitter.lua
+++ lua/plugins/coding/treesitter.lua
@@ -1,3 +1,12 @@
+-- ************************************************************************
+-- * GitHub: https://github.com/nvim-treesitter/nvim-treesitter/tree/main *
+-- *                                                                      *
+-- * Tree-sitter analyse le code via un arbre syntaxique pour fournir une *
+-- * coloration syntaxique, l'indentation fiable et éventuellement un     *
+-- * folding.                                                             *
+-- ************************************************************************
+
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -6,12 +15,13 @@ return {
     build = ":TSUpdate", -- installe et met à jour les parsers
     config = function()
       local ok, ts_configs = pcall(require, "nvim-treesitter.configs")
-      if not ok then return end
+      if not ok then return end  -- si plugin pas disponible
 
       ts_configs.setup({
         ensure_installed = { "lua", "python", "rust", "toml" },
         highlight = { enable = true },
         indent = { enable = true },
+        auto_install = true,  -- Pour les langages non listés dans ensure_installed
       })
     end,
   }
