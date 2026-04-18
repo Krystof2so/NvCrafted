e6d7825e8b7ece81b18b1650079ab7b4f08e564d
Krystof2so <christo.2so@gmail.com>
2026-04-18 18:03:27 +0200

    feat(fix): updates ‘options.lua’ following the upgrade from Neovim v0.11.6 to v0.12.1
    - ‘vim.lsp.log.set_level(“error”)’ instead of 'vim.lsp.set_log_level(“error”)'

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -91,5 +91,5 @@ opt.foldcolumn = "0" -- Masque la colonne de repliement
 
 -- [[ LSP ]]
 -- Limiter les logs LSP uniquement aux erreurs (évite des fichiers de log volumineux)
-vim.lsp.set_log_level("error")
+vim.lsp.log.set_level("error")
 -- Si fichier trop volumineux => rm ~/.local/state/nvim/lsp.log
