5683ea116f67e2e8406b0f6db1d6708c7c8ea399
Krystof2so <christo.2so@gmail.com>
2026-05-05 23:52:57 +0200

    feat(plugins): add the comment.nvim plugin

---
+++ lua/plugins/coding/comment_nvim.lua
@@ -0,0 +1,13 @@
+-- ****************************************************
+-- * Github: https://github.com/numToStr/Comment.nvim *
+-- *                                                  *
+-- * Plugin de commentaires intelligent et            *
+-- * performant pour Neovim                           *
+-- ****************************************************
+
+return {
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+}
