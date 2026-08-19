3ccd18cbf2b5e68b34b0dfa847215d7925248c78
Krystof2so <52052603+Krystof2so@users.noreply.github.com>
2026-08-19 15:27:08 +0200

    feat(lualine): displays pending commands via noice.nvim

--- lua/plugins/appearence/lualine.lua
+++ lua/plugins/appearence/lualine.lua
@@ -6,53 +6,62 @@
 -- **********************************************************
 
 return {
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function()
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			vim.api.nvim_set_hl(0, "LualineModified", { fg = "#D08770" }) -- highlight du symbole "modifié"
 
-            vim.api.nvim_set_hl(0, 'LualineModified', { fg = '#D08770' })  -- highlight du symbole "modifié"                    
+			local noice_status = require("noice").api.status
 
-            require('lualine').setup({
-                options = {
-                    theme = 'auto',                                   -- Thème automatique basé sur le colorscheme
-                    component_separators = { left = '', right = ''},
-                    section_separators = { left = '', right = ''},
-                    icons_enabled = true,                             -- Activer les icônes
-                },
-                sections = {
-                    lualine_a = {'mode'},
-                    lualine_b = {'branch', 'diff', 'diagnostics'},
-                    lualine_c = {
-                        {  -- Affichage du type de fichier
-                            'filetype',
-                            colored = true,                           -- Pour colorer l'icône
-                            icon_only = true,                         -- n'affiche que l'icône
-                        },
-                        { -- Affichage du nom de fichier avec symbole modifié ou lecture seule si besoin
-                            'filename',
-                            symbols = {
-                                modified = "%#LualineModified#●", -- Pour indiquer que le fichier est modifié
-                                readonly = "🔒"                   -- Pour indiquer que le fichier est en lecture seule
-                            },
-                        },
-                    },
-                    lualine_x = {'location'},
-                    lualine_y = {'progress'},
-                    lualine_z = {
-                        {
-                            function()
-                                return os.date('%H:%M')  -- Affiche l'heure en format 24h
-                            end,
-                            icon = ' ',
-                        }
-                    }
-                },
-                inactive_sections = {
-                    lualine_c = {'filename'},
-                    lualine_x = {'location'},
-                },
-            })
-        end
-    },
+			require("lualine").setup({
+				options = {
+					theme = "auto", -- Thème automatique basé sur le colorscheme
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					icons_enabled = true, -- Activer les icônes
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = {
+						{ -- Affichage du type de fichier
+							"filetype",
+							colored = true, -- Pour colorer l'icône
+							icon_only = true, -- n'affiche que l'icône
+						},
+						{ -- Affichage du nom de fichier avec symbole modifié ou lecture seule si besoin
+							"filename",
+							symbols = {
+								modified = "%#LualineModified#●", -- Pour indiquer que le fichier est modifié
+								readonly = "🔒", -- Pour indiquer que le fichier est en lecture seule
+							},
+						},
+					},
+					lualine_x = {
+						{
+							---@diagnostic disable-next-line: undefined-field
+							noice_status.command.get,
+							---@diagnostic disable-next-line: undefined-field
+							cond = noice_status.command.has,
+						},
+                        { 'location' },
+					},
+					lualine_y = { "progress" },
+					lualine_z = {
+						{
+							function()
+								return os.date("%H:%M") -- Affiche l'heure en format 24h
+							end,
+							icon = " ",
+						},
+					},
+				},
+				inactive_sections = {
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+				},
+			})
+		end,
+	},
 }
